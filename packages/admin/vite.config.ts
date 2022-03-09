c64f3df19c43cb396f6f76024a09137bb8e039fc
IvanMOA <ax3l.imo@gmail.com>
2022-03-08 21:33:14 -0600

    Se mejora index.html y se customiza tema de element-plus

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -1,13 +1,12 @@
-import * as path from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import pkg from './package.json'
-
-process.env.VITE_APP_VERSION = pkg.version
-if (process.env.NODE_ENV === 'production') {
-  process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString()
+import * as path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import pkg from "./package.json";
+import ElementPlus from "unplugin-element-plus/vite";
+process.env.VITE_APP_VERSION = pkg.version;
+if (process.env.NODE_ENV === "production") {
+  process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString();
 }
-
 export default defineConfig({
   plugins: [
     vue({
@@ -15,10 +14,20 @@ export default defineConfig({
         refSugar: true,
       },
     }),
+    ElementPlus({
+      useSource: true,
+    }),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/styles/element-plus/index.scss" as *;`,
+      },
     },
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
