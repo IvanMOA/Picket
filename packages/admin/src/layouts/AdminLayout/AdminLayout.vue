2d16e954d167182b7213c4330887318ba566bc3b
IvanMOA <ax3l.imo@gmail.com>
2022-03-12 16:19:17 -0600

    Se esconden elementos innecesarios para interacción con sidebar en distintas pantallas

--- packages/admin/src/layouts/AdminLayout/AdminLayout.vue
+++ packages/admin/src/layouts/AdminLayout/AdminLayout.vue
@@ -8,17 +8,28 @@ const isSidebarOpen = ref(true);
 <template>
   <main class="bg-gray-100 dark:bg-gray-800 h-screen overflow-hidden relative">
     <div class="flex items-start justify-between">
-      <AdminLayoutSidebar :isOpen="isSidebarOpen" @close="isSidebarOpen = false" />
+      <AdminLayoutSidebar
+        :isOpen="isSidebarOpen"
+        @close="isSidebarOpen = false"
+      />
       <div class="flex flex-col w-full md:space-y-4">
         <header class="w-full h-16 z-40 flex items-center justify-between">
           <div class="block lg:hidden ml-6">
-            <button class="flex p-2 items-center rounded-full bg-white shadow text-gray-500 text-md"
-                    @click="isSidebarOpen = true">
-              <svg class="text-gray-400" fill="currentColor" height="20" viewBox="0 0 1792 1792" width="20"
-                   xmlns="http://www.w3.org/2000/svg">
+            <button
+              class="flex p-2 items-center rounded-full bg-white shadow text-gray-500 text-md sm:hidden"
+              @click="isSidebarOpen = true"
+            >
+              <svg
+                class="text-gray-400"
+                fill="currentColor"
+                height="20"
+                viewBox="0 0 1792 1792"
+                width="20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path
-                  d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z">
-                </path>
+                  d="M1664 1344v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19h-1408q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45z"
+                ></path>
               </svg>
             </button>
           </div>
@@ -30,5 +41,4 @@ const isSidebarOpen = ref(true);
       </div>
     </div>
   </main>
-
 </template>
